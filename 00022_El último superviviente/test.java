d95c4caf477bbb790401be4623979a336e2fdd57
nadia@mumuki.org <nadia@mumuki.org>
2021-11-23 19:27:00 +0000

    Mumuki Export on 2021-11-23 19:27:00 +0000

--- 00022_El último superviviente/test.java
+++ 00022_El último superviviente/test.java
@@ -7,7 +7,7 @@ public void before() {
 
 @Test
 public void ASD_con_vector_1_0_devuelve_D() {
-	Assert.assertEquals("D", pr.ultimoSuperviviente("ASD", new int[] {1, 0});	
+	Assert.assertEquals("D", pr.ultimoSuperviviente("ASD", new int[] {1, 0}));	
 }
 
 @Test
