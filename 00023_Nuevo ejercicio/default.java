7ad278e280c6891bbd2aefe0b6df2a8acc1243a2
nadia@mumuki.org <nadia@mumuki.org>
2021-11-23 19:59:57 +0000

    Mumuki Export on 2021-11-23 19:59:57 +0000

---
+++ 00023_Nuevo ejercicio/default.java
@@ -0,0 +1,32 @@
+class D {
+  public int func(int a) {
+   // Acá escribí la lógica necesaria
+  }
+}
+
+Class A{
+  public ClaseA() {
+  }
+  Protected int func(int a){
+    a = a * 2;
+    return a;
+  }
+}
+
+Class B{
+  public ClaseB() {
+  }
+  Protected int func(int a){
+    a = a * 3;
+    return a;
+  }
+}
+
+Class C{
+  public ClaseC() {
+  }
+  Protected int func(int a){
+    a = a * 5;
+    return a;
+  }
+}
--- 00023_Nuevo ejercicio/test.java
+++ 00023_Nuevo ejercicio/test.java
@@ -1,4 +1,26 @@
+D pr;
+
+@Before
+public void before() {
+  pr = new D();
+}
+
 @Test
-public void testDescriptionExample() {
-  Assert.assertTrue(true);
+public void con_3_se_obtiene_162() {
+	Assert.assertEquals(162, pr.func(3));
+}
+
+@Test
+public void con_4_se_obtiene_512() {
+	Assert.assertEquals(512, pr.func(4));
+}
+
+@Test
+public void con_10_se_obtiene_20000() {
+	Assert.assertEquals(20000, pr.func(5));
+}
+
+@Test
+public void mi_test() {
+	Assert.assertEquals(10290, pr.func(7));
 }
