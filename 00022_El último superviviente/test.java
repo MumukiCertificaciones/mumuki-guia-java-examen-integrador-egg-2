dab2506b14362a5093ddeb11ccc241350550bb81
nadia@mumuki.org <nadia@mumuki.org>
2021-11-23 19:10:49 +0000

    Mumuki Export on 2021-11-23 19:10:49 +0000

--- 00022_El último superviviente/test.java
+++ 00022_El último superviviente/test.java
@@ -3,10 +3,9 @@ Examen pr;
 @Before
 public void before() {
   pr = new Examen();
-  ArrayList<Integer> vector_1 = new ArrayList(Arrays.asList(1, 0));
-}
+ }
 
 @Test
 public void ASD_con_vector_1_0_devuelve_D() {
-	Assert.assertEquals("D", pr.ultimoSuperviviente("ASD", vector_1));	
+	Assert.assertEquals("D", pr.ultimoSuperviviente("ASD", [0,1]));
 }
