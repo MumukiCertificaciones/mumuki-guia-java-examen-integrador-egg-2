157d2046ababd16792bb422173c4b607f3678254
nadia@mumuki.org <nadia@mumuki.org>
2021-11-24 15:22:09 +0000

    Mumuki Export on 2021-11-24 15:22:09 +0000

--- 00024_Excepciones/test.java
+++ 00024_Excepciones/test.java
@@ -5,10 +5,6 @@ public void before() {
   pr = new MiCalculadora();
 }
 
-@Rule
-public ExpectedException exceptionRule = ExpectedException.none();
-
-
 @Test
 public void para_8_a_la_2_devuelve_64() throws Exception {
 	Assert.assertEquals(64, pr.potencia(8, 2), 0);
@@ -21,7 +17,13 @@ public void para_2_a_la_10_devuelve_1024() throws Exception {
 
 @Test
 public void si_n_es_0_lanza_excepcion() {
-    exceptionRule.expect(Exception.class);
-    exceptionRule.expectMessage("n o p no pueden ser negativos");
-    pr.potencia(0, 2);
+    Exception exception = assertThrows(Exception.class, () -> {
+        pr.potencia(0, 2);
+    });
+
+    String expectedMessage = "n o p no pueden ser negativos";
+    String actualMessage = exception.getMessage();
+
+    assertTrue(actualMessage.contains(expectedMessage));
 }
+
