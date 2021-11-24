bc4f91158c5fb9640a3482d78afc6a96b3a7c2eb
nadia@mumuki.org <nadia@mumuki.org>
2021-11-24 12:14:15 +0000

    Mumuki Export on 2021-11-24 12:14:15 +0000

--- 00024_Excepciones/test.java
+++ 00024_Excepciones/test.java
@@ -6,8 +6,13 @@ public void before() {
 }
 
 @Test
-public void para_2_a_la_8_devuelve_64() throws Exception {
+public void para_8_a_la_2_devuelve_64() throws Exception {
 	Assert.assertEquals(64, pr.potencia(8, 2), 0);
+}
+
+@Test
+public void para_2_a_la_10_devuelve_1024() throws Exception {
+	Assert.assertEquals(1024, pr.potencia(2, 10), 0);
 }
 
 @Test(expected = Exception.class)
